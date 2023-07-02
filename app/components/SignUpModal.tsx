573d7525799d81bfda44044c511dd5ef4bd7b8d0
Sorin <sorinnechiforora@gmail.com>
2023-07-02 20:48:36 +0100

    Add basic responsiveness for various screensizes

--- app/components/SignUpModal.tsx
+++ app/components/SignUpModal.tsx
@@ -2,49 +2,61 @@ import Image from "next/image";
 
 export default function SignUpModal() {
   return (
-    <div className="flex overflow-hidden bg-white sm:h-[85%] sm:w-4/6 sm:rounded-xl">
+    <div className="sm:flex sm:h-[85%] sm:w-4/6 sm:justify-center sm:overflow-hidden sm:rounded-xl">
       <div
         id="edt-greeting-section"
-        className="flex items-center px-4 sm:h-full sm:w-[45%] sm:bg-teal-600"
+        className="hidden lg:flex lg:h-full lg:w-[45%] lg:items-center lg:bg-teal-600 lg:px-8"
       >
         <h1 className="text-5xl">
           This is a recreation of the {`RSA's`} My EDT Portal.
         </h1>
       </div>
-      <div className="flex flex-col items-center gap-2 py-4 sm:w-[55%] sm:bg-purple-700">
+      <div className="flex flex-col items-center gap-4 p-4 sm:w-[55%] sm:max-w-max sm:bg-purple-700">
         <Image
           src="/rsa-logo.png"
           width={100}
           height={100}
           alt="RSA logo"
         ></Image>
-        <h2 className="text-3xl">
+        <h2 className="text-3xl font-bold">
           Sign up to begin tracking your EDT lessons.
         </h2>
-        <form className="mt-40 flex flex-col gap-4 sm:h-full sm:w-full">
-          <div>
-            <label htmlFor="first-name-input" className="bg-white">
+        <form className="flex flex-col gap-4 sm:h-full sm:w-full">
+          <div className="flex flex-col">
+            <label
+              htmlFor="first-name-input"
+              className="w-max bg-white text-xl font-semibold"
+            >
               First Name
             </label>
-            <input id="first-name-input" type="text" />
+            <input name="first-name-input" type="text" className="w-52" />
           </div>
-          <div>
-            <label htmlFor="last-name-input" className="bg-white">
+          <div className="flex flex-col">
+            <label
+              htmlFor="last-name-input"
+              className="w-max bg-white text-xl font-semibold"
+            >
               Last Name
             </label>
-            <input id="last-name-input" type="text" />
+            <input name="last-name-input" type="text" className="w-52" />
           </div>
-          <div>
-            <label htmlFor="email-address-input" className="bg-white">
+          <div className="flex flex-col">
+            <label
+              htmlFor="email-address-input"
+              className="w-max bg-white text-xl font-semibold"
+            >
               Email
             </label>
-            <input id="email-address-input" type="text" />
+            <input name="email-address-input" type="text" className="w-72" />
           </div>
-          <div>
-            <label htmlFor="email-address-input" className="bg-white">
+          <div className="flex flex-col">
+            <label
+              htmlFor="password-input"
+              className="w-max bg-white text-xl font-semibold"
+            >
               Password
             </label>
-            <input id="email-address-input" type="text" />
+            <input name="password-input" type="password" className="w-72" />
           </div>
           <div className="self-center">
             <button type="submit" className="bg-white ">
