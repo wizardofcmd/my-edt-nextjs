839f57e8fe646c2aa3fd84ce8a08e1ab5fbf3218
Sorin <sorinnechiforora@gmail.com>
2023-07-06 20:47:59 +0100

    Improve colour scheme and responsiveness for basic SignUpModal

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import SignUpModal from "./components/SignUpModal";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen bg-fuchsia-300 sm:flex sm:items-center sm:justify-center sm:bg-emerald-400">
+    <main className="h-screen w-screen sm:flex sm:items-center sm:justify-center sm:bg-slate-400">
       <SignUpModal />
     </main>
   );
