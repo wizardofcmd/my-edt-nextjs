ea885f4b838aec1fe04670d637aa5617cb90f355
wizardofcmd <sorinnechiforora@gmail.com>
2023-12-08 16:13:51 +0000

    Adjust flex styles on opening modal

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,26 @@ import { SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen sm:flex sm:items-center sm:justify-center sm:bg-slate-400">
-      <div className="sm:flex sm:h-[85%] sm:w-4/6 sm:justify-center sm:overflow-hidden sm:rounded-xl">
-        <div className="flex h-screen w-full flex-col items-center justify-center gap-4 p-4 sm:h-full sm:rounded-lg sm:bg-white">
-          <Image
-            src="/rsa-logo.png"
-            width={100}
-            height={100}
-            alt="RSA logo"
-            unoptimized
-          ></Image>
-          <h1 className="hidden text-center sm:flex sm:text-3xl sm:font-bold">
-            Start tracking your EDT lessons, today.
-          </h1>
+    <main className="flex h-screen w-screen items-center justify-center sm:bg-slate-400">
+      <div className="flex h-full w-full items-center justify-center justify-items-center sm:h-2/3 sm:w-2/3">
+        <div className="flex h-full w-full flex-col items-center justify-center gap-4 p-4 sm:rounded-lg sm:bg-white">
+          <div className="flex flex-col items-center">
+            <Image
+              src="/rsa-logo.png"
+              width={100}
+              height={100}
+              alt="RSA logo"
+              unoptimized
+            ></Image>
+            <h1 className="hidden text-center sm:flex sm:text-3xl sm:font-bold">
+              Start tracking your EDT lessons, today.
+            </h1>
+          </div>
           <div className="flex h-1/6 w-9/12 flex-col justify-center gap-4 sm:h-[8%] sm:flex-row">
             <SignUpButton redirectUrl="/dashboard">
               <button
                 type="button"
-                className="text-md flex h-full w-full items-center justify-center whitespace-nowrap rounded-md bg-indigo-600 px-3 py-1.5 font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 md:h-full md:min-h-[64px] md:w-4/5"
+                className="text-md flex h-full min-h-[3.5rem] w-full items-center justify-center whitespace-nowrap rounded-md bg-indigo-600 px-3 py-1.5 text-xl font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 md:h-full md:w-4/5"
               >
                 Sign Up
               </button>
@@ -29,7 +31,7 @@ export default function Home() {
             <SignInButton redirectUrl="/dashboard">
               <button
                 type="button"
-                className="text-md flex h-full w-full items-center justify-center whitespace-nowrap rounded-md bg-indigo-600 px-3 py-1.5 font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 md:h-full md:min-h-[64px] md:w-4/5"
+                className="text-md flex h-full min-h-[3.5rem] w-full items-center justify-center whitespace-nowrap rounded-md bg-indigo-600 px-3 py-1.5 text-xl font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 md:h-full md:w-4/5"
               >
                 Login
               </button>
