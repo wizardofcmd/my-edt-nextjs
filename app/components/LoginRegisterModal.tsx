bd0b8b50cb04436783c0c81b516a0a9260851d25
wizardofcmd <sorinnechiforora@gmail.com>
2024-05-09 18:42:40 +0100

    Move login and register to component and redirect user if logged in

---
+++ app/components/LoginRegisterModal.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { SignUpButton } from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
+
+export default function LoginRegisterModal() {
+  return (
+    <main className="flex h-screen min-h-screen w-screen items-center justify-center py-8 sm:bg-slate-400 md:overflow-scroll">
+      <div className="flex h-full w-full items-center justify-center justify-items-center sm:h-2/3 sm:w-2/3">
+        <div className="flex min-h-full w-full flex-col items-center justify-center p-6 sm:rounded-lg sm:bg-white">
+          <div className="flex h-full flex-col justify-end gap-4 pb-4 sm:items-center sm:justify-center">
+            <Image
+              src="/rsa-logo.png"
+              width={100}
+              height={100}
+              alt="Road Safety Authority logo"
+              unoptimized
+              className={"min-h-[100px] min-w-[100px]"}
+            ></Image>
+            <h1 className="hidden text-center sm:flex sm:text-3xl sm:font-bold xl:text-4xl">
+              Start tracking your EDT lessons today.
+            </h1>
+          </div>
+          <div className="flex h-full w-full flex-col gap-6 lg:flex-row lg:justify-center">
+            <SignInButton redirectUrl="/dashboard">
+              <button
+                type="button"
+                className="flex h-1/5 w-full min-w-[100px] max-w-[450px] items-center justify-center self-center whitespace-nowrap rounded-md bg-indigo-600 px-3 py-6 text-xl font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2  focus-visible:outline-indigo-600 sm:h-2/3 sm:text-2xl lg:self-auto lg:text-3xl"
+              >
+                Login
+              </button>
+            </SignInButton>
+            <SignUpButton redirectUrl="/dashboard">
+              <button
+                type="button"
+                className="flex h-1/5 w-full min-w-[100px] max-w-[450px] items-center justify-center self-center whitespace-nowrap rounded-md bg-indigo-600 px-3 py-6 text-xl font-semibold leading-6 text-white  shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:h-2/3 sm:text-2xl lg:self-auto lg:text-3xl"
+              >
+                Sign Up
+              </button>
+            </SignUpButton>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
