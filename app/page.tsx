01f61b291dcf5cbeac3dcf24f46faa28ccd297a6
wizardofcmd <sorinnechiforora@gmail.com>
2024-05-09 13:07:55 +0100

    Fix height sizing of container div for all screens

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen items-center justify-center sm:bg-slate-400">
+    <main className="flex min-h-screen w-screen items-center justify-center py-8 sm:bg-slate-400">
       <div className="flex h-full w-full items-center justify-center justify-items-center sm:h-2/3 sm:w-2/3">
-        <div className="flex h-full w-full flex-col items-center p-6 sm:rounded-lg sm:bg-white">
+        <div className="flex min-h-full w-full flex-col items-center justify-center p-6 sm:rounded-lg sm:bg-white">
           <div className="flex h-full flex-col justify-end gap-4 pb-4 sm:items-center sm:justify-center">
             <Image
               src="/rsa-logo.png"
@@ -16,7 +16,7 @@ export default function Home() {
               unoptimized
               className={"min-h-[100px] min-w-[100px]"}
             ></Image>
-            <h1 className="hidden text-center sm:flex sm:text-3xl sm:font-bold">
+            <h1 className="hidden text-center sm:flex sm:text-3xl sm:font-bold xl:text-4xl">
               Start tracking your EDT lessons today.
             </h1>
           </div>
@@ -24,7 +24,7 @@ export default function Home() {
             <SignInButton redirectUrl="/dashboard">
               <button
                 type="button"
-                className="flex h-1/5 w-full min-w-[100px] max-w-[450px] items-center justify-center self-center whitespace-nowrap rounded-md bg-indigo-600 px-3 py-6 text-xl font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2  focus-visible:outline-indigo-600 sm:h-2/3 sm:text-2xl lg:self-auto"
+                className="flex h-1/5 w-full min-w-[100px] max-w-[450px] items-center justify-center self-center whitespace-nowrap rounded-md bg-indigo-600 px-3 py-6 text-xl font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2  focus-visible:outline-indigo-600 sm:h-2/3 sm:text-2xl lg:self-auto lg:text-3xl"
               >
                 Login
               </button>
@@ -32,7 +32,7 @@ export default function Home() {
             <SignUpButton redirectUrl="/dashboard">
               <button
                 type="button"
-                className="flex h-1/5 w-full min-w-[100px] max-w-[450px] items-center justify-center self-center whitespace-nowrap rounded-md bg-indigo-600 px-3 py-6 text-xl font-semibold leading-6 text-white  shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:h-2/3 sm:text-2xl lg:self-auto"
+                className="flex h-1/5 w-full min-w-[100px] max-w-[450px] items-center justify-center self-center whitespace-nowrap rounded-md bg-indigo-600 px-3 py-6 text-xl font-semibold leading-6 text-white  shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:h-2/3 sm:text-2xl lg:self-auto lg:text-3xl"
               >
                 Sign Up
               </button>
