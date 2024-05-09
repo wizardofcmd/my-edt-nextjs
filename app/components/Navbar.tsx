ef5f6c60e99ee20c5d761a3c2fe861ee4dc8348b
wizardofcmd <sorinnechiforora@gmail.com>
2024-05-09 15:03:49 +0100

    Create BurgerIcon component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,41 @@
+"use client";
+
 import Image from "next/image";
 import RSALogo from "/public/rsa-logo.png";
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
+import { useState } from "react";
+import BurgerIcon from "./BurgerIcon";
 
 export default function Navbar() {
+  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+
+  const toggleBurgerMenu = () => setIsBurgerMenuOpen(!isBurgerMenuOpen);
+  const NavbarLinks = () => (
+    <>
+      <div className="ml-16 flex items-center gap-16">
+        <Link href="/dashboard" className="text-lg font-medium text-white">
+          Dashboard
+        </Link>
+        <Link href="#" className="text-lg font-medium text-white">
+          My Licenses
+        </Link>
+        <Link href="#" className="text-lg font-medium text-white">
+          Find an Instructor
+        </Link>
+      </div>
+      <div className="ml-auto flex sm:pl-8">
+        <UserButton />
+      </div>
+    </>
+  );
+
   return (
-    <header className="flex h-16 bg-slate-900">
+    <header
+      className={`flex min-h-max w-full  bg-slate-900 ${
+        isBurgerMenuOpen ? "flex-col" : ""
+      }`}
+    >
       {/* Navbar for bigger screens */}
       <div className="hidden w-full items-center px-4 sm:pl-8 md:flex md:pl-12 lg:pl-16">
         <Link href="/dashboard" className="flex h-5/6 w-auto items-center">
@@ -16,44 +46,22 @@ export default function Navbar() {
             unoptimized
           ></Image>
         </Link>
-        <div className="ml-16 flex items-center gap-16">
-          <Link href="/dashboard" className="text-lg font-medium text-white">
-            Dashboard
-          </Link>
-          <Link href="#" className="text-lg font-medium text-white">
-            My Licenses
-          </Link>
-          <Link href="#" className="text-lg font-medium text-white">
-            Find an Instructor
-          </Link>
-        </div>
-        <div className="ml-auto flex sm:pl-8">
-          <UserButton />
-        </div>
+        <NavbarLinks />
       </div>
 
       {/* Mobile menu icon */}
-      <div className="flex items-center px-4 md:hidden">
-        <button
-          type="button"
-          className="items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-        >
-          <svg
-            className="block h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-        </button>
+      <div
+        className="relative flex items-center p-2 md:hidden"
+        onClick={toggleBurgerMenu}
+      >
+        <BurgerIcon />
       </div>
+
+      {isBurgerMenuOpen && (
+        <div>
+          <NavbarLinks />
+        </div>
+      )}
     </header>
   );
 }
