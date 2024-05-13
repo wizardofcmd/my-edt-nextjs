a4ce359b5908927c56e544dc9d794592972180d6
wizardofcmd <sorinnechiforora@gmail.com>
2024-05-13 16:22:03 +0100

    Update card flex direction for various screens on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,14 +12,14 @@ export default function Dashboard() {
   if (isSignedIn) {
     return (
       <div className="h-full w-full">
-        <h1 className="font-bold text-white md:text-4xl">
+        <h1 className="hidden font-bold text-white md:flex md:pb-2 md:text-4xl">
           {`Welcome, ${user.fullName}.`}
         </h1>
-        <div className="flex flex-col sm:flex-row">
-          <div className="w-3/5">
+        <div className="flex w-full flex-col gap-4 lg:flex-row">
+          <div className="w-full lg:w-3/5">
             <LatestLessons />
           </div>
-          <div className="w-2/5">
+          <div className="flex w-full flex-col gap-4 lg:w-2/5">
             <RSANews />
             <div className="min-w-max">
               <PersonalCalendar />
