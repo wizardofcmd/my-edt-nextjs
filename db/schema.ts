d9b699caf0931311f65386fee8a9c932d0d89c13
wizardofcmd <sorinnechiforora@gmail.com>
2024-09-11 15:16:37 +0100

    Create bwLessons table

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 import {
+  boolean,
   integer,
   pgTable,
   serial,
@@ -32,4 +33,22 @@ export const licenses = pgTable("licenses", {
   id: serial("id").primaryKey(),
   user_id: integer("user_id").references(() => users.id),
   type: text("type").notNull(),
+  full_license: boolean("full_license").default(false).notNull(),
+});
+
+export const bwLessons = pgTable("bwLessons", {
+  id: serial("id").primaryKey(),
+  user_id: integer("user_id").references(() => users.id),
+  lesson_1: boolean("lesson_1").default(false).notNull(),
+  lesson_2: boolean("lesson_2").default(false).notNull(),
+  lesson_3: boolean("lesson_3").default(false).notNull(),
+  lesson_4: boolean("lesson_4").default(false).notNull(),
+  lesson_5: boolean("lesson_5").default(false).notNull(),
+  lesson_6: boolean("lesson_6").default(false).notNull(),
+  lesson_7: boolean("lesson_7").default(false).notNull(),
+  lesson_8: boolean("lesson_8").default(false).notNull(),
+  lesson_9: boolean("lesson_9").default(false).notNull(),
+  lesson_10: boolean("lesson_10").default(false).notNull(),
+  lesson_11: boolean("lesson_11").default(false).notNull(),
+  lesson_12: boolean("lesson_12").default(false).notNull(),
 });
