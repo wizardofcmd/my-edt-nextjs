778d1178890be22b8446bbb44cb679f7f82a7ad3
wizardofcmd <sorinnechiforora@gmail.com>
2024-09-12 13:15:30 +0100

    Fix path for envConfig for drizzle

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,11 @@
-import "@/drizzle/envConfig";
+import "@/db/envConfig";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 import * as schema from "./schema";
-
 export const db = drizzle(sql, { schema });
 
 export const getUsers = async () => {
-  return db.query.users.findMany();
+  const results = db.query.users.findMany();
+
+  return results;
 };
