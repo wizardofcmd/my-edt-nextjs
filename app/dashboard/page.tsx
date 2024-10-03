e60e29cb6a9de45bb641ea94aa111fede3abf84d
wizardofcmd <sorinnechiforora@gmail.com>
2024-10-03 14:38:14 +0100

    Delete UserTest component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,7 @@
 "use server";
 
 import DashboardItems from "@/components/Dashboard/DashboardItems";
-import UserTest from "@/components/Dashboard/UserTest";
 
 export default async function Dashboard() {
-  return (
-    <>
-      <UserTest />
-      <DashboardItems />
-    </>
-  );
+  return <DashboardItems />;
 }
