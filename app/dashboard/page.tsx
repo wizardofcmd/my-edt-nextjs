be78dcc772ac59aa964e7552badc3e760223b5b9
wizardofcmd <sorinnechiforora@gmail.com>
2024-10-07 13:50:24 +0100

    Call getBwLessons after retrieving user from db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,23 @@
 "use server";
 
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 
 import DashboardItems from "@/components/Dashboard/DashboardItems";
-import { getUser } from "@/db";
+import { getBwLessons, getUser } from "@/db";
 
 export default async function Dashboard() {
   const { userId } = auth();
 
+  // Clerk User ID
   if (userId) {
     const user = await getUser(userId);
 
     console.log("server - user:", user);
+
+    if (user) {
+      const lessons = await getBwLessons(user.id);
+      console.log("server - lessons:", lessons);
+    }
   }
 
   return <DashboardItems />;
--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,24 @@
 import "@/db/envConfig";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
+import { bwLessons } from "./schema";
 import * as schema from "./schema";
 import { eq } from "drizzle-orm";
 export const db = drizzle(sql, { schema });
 
-export const getUser = async (userId: string) => {
+export const getUser = async (clerkUserId: string) => {
   const user = await db.query.users.findFirst({
-    where: (user, { eq }) => eq(user.clerk_user_id, userId),
+    where: (user, { eq }) => eq(user.clerk_user_id, clerkUserId),
   });
 
   return user;
+};
+
+export const getBwLessons = async (userId: number) => {
+  const lessons = await db
+    .select()
+    .from(bwLessons)
+    .where(eq(bwLessons.user_id, userId));
+
+  return lessons;
 };
